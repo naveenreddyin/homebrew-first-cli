0150ae1071014b14a831a4cecd06307c53314d6b
VASUDEVAN Naveen (EXT-RIC-CH) <naveen.vasudevan@VASUDEVANs-MacBook-Pro.local>
2022-10-25 15:15:02 +0200

    feat: taking command line argument

--- Formula/first-cli.rb
+++ Formula/first-cli.rb
@@ -2,8 +2,8 @@ class FirstCli < Formula
     desc "A dummy first cli in rust"
     homepage "https://github.com/naveenreddyin/first-cli/"
     url "https://github.com/naveenreddyin/first-cli/releases/download/v0.1.0/first-cli-mac.tar.gz"
-    sha256 "52548d5548445b1cb4c35fe81fc88da67dd5dbed72e9862724ad7e00d283b962"
-    version "0.1.0"
+    sha256 "7783f3be3e305a3b4828aa98a57925322df7d96bd451b031123c4dc385855b2d"
+    version "0.1.1"
   
     def install
       bin.install "first_cli"
