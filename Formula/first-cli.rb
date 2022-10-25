442a89a4c00c0adf4d7c467a3a5cb15b9ab9fd29
VASUDEVAN Naveen (EXT-RIC-CH) <naveen.vasudevan@VASUDEVANs-MacBook-Pro.local>
2022-10-25 15:43:38 +0200

    fix: updated wrong file tar

--- Formula/first-cli.rb
+++ Formula/first-cli.rb
@@ -2,7 +2,7 @@ class FirstCli < Formula
     desc "A dummy first cli in rust"
     homepage "https://github.com/naveenreddyin/first-cli/"
     url "https://github.com/naveenreddyin/first-cli/releases/download/v0.2.0/first-cli-mac.tar.gz"
-    sha256 "69141b9f10291cbc0a0a05e251ff1002916f9172f55d8d38cf0110bd1069626a"
+    sha256 "49731e0bf5637307c8a0f367581d3e13f441dd804787de3fd37f4e7284bf54d2"
     version "0.2.0"
   
     def install
