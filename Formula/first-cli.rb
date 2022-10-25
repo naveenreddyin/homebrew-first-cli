b8be6773af4feb5f4adb13c4e2e4dc7801e7f727
VASUDEVAN Naveen (EXT-RIC-CH) <naveen.vasudevan@VASUDEVANs-MacBook-Pro.local>
2022-10-25 15:38:51 +0200

    feat: v0.2.0, with async main added

--- Formula/first-cli.rb
+++ Formula/first-cli.rb
@@ -1,11 +1,12 @@
 class FirstCli < Formula
     desc "A dummy first cli in rust"
     homepage "https://github.com/naveenreddyin/first-cli/"
-    url "https://github.com/naveenreddyin/first-cli/releases/download/v0.1.1/first-cli-mac.tar.gz"
-    sha256 "7783f3be3e305a3b4828aa98a57925322df7d96bd451b031123c4dc385855b2d"
-    version "0.1.1"
+    url "https://github.com/naveenreddyin/first-cli/releases/download/v0.2.0/first-cli-mac.tar.gz"
+    sha256 "69141b9f10291cbc0a0a05e251ff1002916f9172f55d8d38cf0110bd1069626a"
+    version "0.2.0"
   
     def install
       bin.install "first_cli"
     end
   end
+  
