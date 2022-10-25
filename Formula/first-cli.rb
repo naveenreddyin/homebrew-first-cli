114acab1194fc72b109042f155658213a20889ca
VASUDEVAN Naveen (EXT-RIC-CH) <naveen.vasudevan@VASUDEVANs-MacBook-Pro.local>
2022-10-25 15:22:30 +0200

    fix: changed url

--- Formula/first-cli.rb
+++ Formula/first-cli.rb
@@ -1,7 +1,7 @@
 class FirstCli < Formula
     desc "A dummy first cli in rust"
     homepage "https://github.com/naveenreddyin/first-cli/"
-    url "https://github.com/naveenreddyin/first-cli/releases/download/v0.1.0/first-cli-mac.tar.gz"
+    url "https://github.com/naveenreddyin/first-cli/releases/download/v0.1.1/first-cli-mac.tar.gz"
     sha256 "7783f3be3e305a3b4828aa98a57925322df7d96bd451b031123c4dc385855b2d"
     version "0.1.1"
   
